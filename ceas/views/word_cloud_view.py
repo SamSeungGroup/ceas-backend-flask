dea0cb6ac6f13604bcceb434552a8d2764f5bede
kws9208 <kws9208@gmail.com>
2023-03-18 23:49:07 +0900

    refactor: 상품 삭제시 워드클라우드 이미지 삭제

--- ceas/views/word_cloud_view.py
+++ ceas/views/word_cloud_view.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, send_file
+from flask import Blueprint, send_file, request
 
 from modules.comment_db import get_comments
 from modules.word_cloud import get_word_cloud
@@ -7,20 +7,24 @@ import os
 
 bp = Blueprint('word-cloud', __name__, url_prefix='/word-cloud')
 
-@bp.route('/<int:product_id>')
+@bp.route('/<int:product_id>', methods=["GET", "DELETE"])
 def comments(product_id):
     file_name = f'images\wordcloud{product_id}.jpeg'
-    result = get_comments(product_id)
-    if not result.get("comments"):
-        return {"error": "No comments exist"}
-    else:
-        total_string = ""
-        for comment in result['comments']:
-            total_string += comment[0]
-        word_cloud = get_word_cloud(total_string)
+    if request.method == "GET":
+        result = get_comments(product_id)
+        if not result.get("comments"):
+            return {"error": "No comments exist"}
+        else:
+            total_string = ""
+            for comment in result['comments']:
+                total_string += comment[0]
+            word_cloud = get_word_cloud(total_string)
 
-        if not os.path.isdir("ceas\images"):
-            os.mkdir("ceas\images")
-        word_cloud.to_file(f'ceas/{file_name}')
+            if not os.path.isdir("ceas\images"):
+                os.mkdir("ceas\images")
+            word_cloud.to_file(f'ceas/{file_name}')
 
-    return send_file(file_name, mimetype='image/jpeg')
+        return send_file(file_name, mimetype='image/jpeg')
+    if request.method == "DELETE":
+        os.remove(f'ceas/{file_name}')
+        return "1"
